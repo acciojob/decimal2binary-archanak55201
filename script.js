c7dc29de1a4f27b19fb757976341e6322059ea79
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-23 22:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function decimalToBinary(num) {
+function decimalToBinary(number) {
   //Write you code here
-  
+
+    //Write you code here
+  let str="";
+      while(number>0){
+        // console.log(Math.floor(number%2));
+        rem=Math.floor(number%2);
+        str+=rem;
+        number=Math.floor(number/2);
+    }
+      console.log(str);
+   
+	return str;
 }
 
 window.decimalToBinary = decimalToBinary;
